c969218e05b430bb2d24d13a6388b9cbc52b7999
bpopov <byurinov1@gmail.com>
2018-06-12 02:03:30 +0200

    [WIP] Implementing unicode unit tests

--- unit_tests.c
+++ unit_tests.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 14:38:49 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/11 20:34:07 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/06/12 02:00:55 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,28 @@ void	test_wc(void)
 	char	*s1;
 	wchar_t	lol;
 	int		i;
-	char	tmp[BUFF_SIZE + 1];
+	wchar_t	tmp[BUFF_SIZE / sizeof(wchar_t)];
 	t_buff	buff;
 
 	i = 0;
-	lol = L'a';
+	lol = L'字';
 	setlocale(P_ALL, "");
-	while (i < BUFF_SIZE)
+	ft_bzero(tmp, BUFF_SIZE);
+	while (i < (BUFF_SIZE / sizeof(wchar_t)))
 	{
-		i += sprintf(tmp + i, "%C", lol);
+		//i += sprintf(tmp + i, "%C", lol);
+		tmp[i] = lol;
+		i++;
 		lol++;
 	}
-	tmp[BUFF_SIZE] = '\0';
+	tmp[BUFF_SIZE / sizeof(wchar_t) - 1] = '\0';
 
-	printf("X %s X", tmp);
+	printf("%S\n", tmp);
 
 	ft_bzero(buff.buff, BUFF_SIZE);
 	ptf_wcs((wchar_t*)tmp, &buff);
-	printf("%s", buff.buff);
-	//TEST_CHECK_(ft_strcmp(s1, buff.buff) == 0, "Expected %s got %s", s1, buff.buff);
+	printf("%s\n", buff.buff);
+	TEST_CHECK_(ft_strcmp(tmp, buff.buff) == 0, "Expected %s got %s", tmp, buff.buff);
 }
 
 TEST_LIST = {
