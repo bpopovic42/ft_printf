2d0c9367afdfac1ecc94e4f5f961addc0316d34e
bpopov <byurinov1@gmail.com>
2018-06-26 00:31:36 +0200

    [WIP] Getting rid of mallocs, improving project base

--- ft_printf/srcs/ft_printf.c
+++ ft_printf/srcs/ft_printf.c
@@ -6,81 +6,186 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 15:26:54 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/12 17:32:03 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/06/26 00:22:59 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int			parse_input(char *fmt, t_buff *buff, va_list ap);
+int			buff_append(t_buff *buff, char *input, int size);
+int			treat_arg(t_buff *buff, char **input, va_list ap);
+int			ft_strprepc(char *str, char c);
+int			is_fspecif(int c);
+int			treat_arg_type_int(t_buff *buff, char type, va_list ap);
+int			ptf_itoa(char *buff, int64_t val);
+int			treat_arg_type_str(t_buff *buff, char type, va_list ap);
+
 int			ft_printf(const char * restrict format, ...)
 {
 	va_list		ap;
-	char		*line;
 	size_t		line_size;
 	char		*ptr;
+	t_buff		buff;
 
 	ptr = (char*)format;
 	va_start(ap, format);
-	line_size = parse_format_string(&line, ap, ptr);
-	write(1, line, line_size);
-
-	return (0);
+	ft_bzero(buff.buff, BUFF_SIZE + 1);
+	buff.pos = 0;
+	line_size = parse_input(ptr, &buff, ap);
+	write(1, buff.buff, buff.pos);
+	return (line_size);
 }
 
-/*
-** Reads from the format string (fmt)
-** Takes the address of a string for the treated input sections to be appended to
-** Returns the total size of final treated output
-*/
-
-size_t		parse_format_string(char **output, va_list ap, char *fmt)
+int			parse_input(char *fmt, t_buff *buff, va_list ap)
 {
-	size_t	flen;
-	size_t	olen;
+	int		read;
 
-	flen = 0;
-	olen = 0;
-	while (*fmt && fmt[flen])
+	read = 0;
+	while (*fmt)
 	{
-		if (fmt[flen] == '%')
+		if (*fmt != '%')
 		{
-			if ((fast_append(output, &fmt, &olen, &flen)) < 0)
-				return 1; //ERROR
-			ft_putendl(*output);
-			if ((parse_format(output, &fmt, ap, &flen, &olen)) < 0)
-				return 1; //ERROR
-			ft_putendl(*output);
+			buff_append(buff, fmt, 1);
+			fmt++;
+			read++;
 		}
-		flen++;
+		else
+		{
+			read += treat_arg(buff, &fmt, ap);
+		}
 	}
-	return (olen);
+	return (read);
 }
 
-int		parse_format(char **output, char **format, va_list ap, size_t *in_len, size_t *out_len)
+int			treat_arg(t_buff *buff, char **input, va_list ap)
 {
-	(void)ap;
-	(void)format;
-	(void)in_len;
-	char		*arg;
-	size_t		format_len;
-	size_t		arg_len;
+	int			i;
+	int			size;
 
-	format_len = get_format_arg(*format, &arg, ap);
-	arg_len = ft_strlen(arg);
-	if ((fast_append(output, &arg, out_len, &arg_len)) < 0)
-		return (-1);
-	*format += format_len;
-	return (0);
+	i = 1;
+	size = 0;
+	if ((*input)[i] == '%')
+		buff_append(buff, *input + i, 1);
+	else
+	{
+		while (!is_fspecif((*input)[i]))
+			i++;
+	}
+	if (ft_strchr("sS", (*input)[i]))
+		size += treat_arg_type_str(buff, (*input)[i], ap);
+	else if (ft_strchr("pdDioOuUxXcC", (*input)[i]))
+		size += treat_arg_type_int(buff, (*input)[i], ap);
+	*input += i + 1;
+	return (size);
 }
 
-int		is_fspecif(int c)
+int			treat_arg_type_str(t_buff *buff, char type, va_list ap)
+{
+	char	*ptr;
+	int		size;
+
+	size = 0;
+	if (type == 's')
+	{
+		ptr = va_arg(ap, char *);
+		size = buff_append(buff, ptr, ft_strlen(ptr));
+	}
+	return (size);
+}
+
+int			treat_arg_type_int(t_buff *buff, char type, va_list ap)
+{
+	char	ptr[19];
+	int		size;
+
+	size = 1;
+	ft_bzero(ptr, 19);
+	if (type == 'd' || type == 'i')
+		size = ptf_itoa(ptr, va_arg(ap, int64_t));
+	else if (type == 'c')
+		ptr[0] = va_arg(ap, int);
+	buff_append(buff, ptr, size);
+	return (size);
+}
+
+void		print_bits(int64_t val)
+{
+	ft_putendl("BITS :");
+	int size;
+
+	size = (sizeof(val) * 8) - 1;
+	while (size)
+	{
+		if ((val >> size))
+		{
+			ft_putchar('1');
+			val ^= ((val >> size) << size);
+		}
+		else
+			ft_putchar('0');
+		size--;
+	}
+	ft_putchar('\n');
+}
+
+int			ptf_itoa(char *buff, int64_t val)
+{
+	int		size;
+	int		is_neg;
+
+	size = 0;
+	is_neg = val < 0 ? 1 : 0;
+	if (val < 0)
+	{
+		val = ~val;
+		size++;
+		val++;
+	}
+	else if (val == 0)
+	{
+		ft_strcinsert(buff, '0', 0);
+		return (1);
+	}
+	while (val > 0)
+	{
+		ft_strcinsert(buff, (val % 10) + '0', 0);
+		val /= 10;
+		size++;
+	}
+	if (is_neg)
+		ft_strcinsert(buff, '-', 0);
+	return (size);
+}
+
+int			buff_append(t_buff *buff, char *input, int size)
+{
+	int i;
+
+	i = 0;
+	if (buff->pos + size >= BUFF_SIZE)
+	{
+		write(1, buff->buff, BUFF_SIZE);
+		buff->pos = 0;
+		ft_bzero(buff->buff, BUFF_SIZE + 1);
+	}
+	while (i < size)
+	{
+		buff->buff[buff->pos + i] = input[i];
+		i++;
+	}
+	buff->pos += i;
+	return (i);
+}
+
+int			is_fspecif(int c)
 {
 	return (c == 's' || c == 'S' || c == 'p' || c == 'd' || c == 'D' || c == 'i'
 			|| c == 'o' || c == 'O' || c == 'u' || c == 'U' || c == 'x'
 				|| c == 'X' || c == 'c' || c == 'C');
 }
 
-int		get_format_arg(char *format, char **output, va_list ap)
+int			get_format_arg(char *format, char **output, va_list ap)
 {
 	size_t	i;
 	int		format_len;
@@ -100,33 +205,6 @@ int		get_format_arg(char *format, char **output, va_list ap)
 	else
 		*output = ft_strdup("#Unsupported specifier");
 	return (format_len + 1);
-}
-
-int		fast_append(char **dst, char **app, size_t *dlen, size_t *alen)
-{
-	char *new;
-
-	if (*dst == NULL && *alen != 0)
-		*dst = ft_strndup(*app, *alen);
-	else
-	{
-		if (!(new = ft_strnew(*dlen + *alen)))
-			return (-1);
-		if (*dst != NULL && *dlen > 0)
-		{
-			new = ft_strncpy(new, *dst, *dlen);
-		}
-		if (app != NULL && *alen > 0)
-		{
-			new = ft_strncat(new, *app, *alen);
-			*app += *alen;
-		}
-		*dst = new;
-		new = NULL;
-	}
-	*dlen += *alen;
-	*alen = 0;
-	return ((int)(*dlen));
 }
 
 t_list		*store_args(const char * restrict format, va_list ap)
