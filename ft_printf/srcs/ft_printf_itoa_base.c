a54e035a1a659e49ad81af7ec060f7b642872e1e
Boris POPOVIC <bopopovi@student.42.fr>
2018-06-27 13:29:42 +0200

    [FIX] Resulting string wasn't correctly terminated

--- ft_printf/srcs/ft_printf_itoa_base.c
+++ ft_printf/srcs/ft_printf_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 19:52:34 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/26 20:26:45 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/06/27 13:22:59 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int			ft_printf_itoa_base(char *buff, int base, int64_t nbr)
 	char	*charset = "0123456789abcdef";
 	int		i;
 
-	ft_bzero(buff, 64 + 1);
 	i = 0;
 	if (base > 16 || base < 2)
 		return (-1);
@@ -27,6 +26,7 @@ int			ft_printf_itoa_base(char *buff, int base, int64_t nbr)
 		i++;
 		nbr /= base;
 	}
+	buff[i] = '\0';
 	buff = ft_strrev(buff);
 	return (i);
 }
