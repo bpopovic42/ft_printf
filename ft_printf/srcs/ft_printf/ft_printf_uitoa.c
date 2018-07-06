86a3c06dfe5c5fd9e195ca1c83321a2f723e8009
Boris POPOVIC <bopopovi@student.42.fr>
2018-07-06 14:50:03 +0200

    [FIX] Uitoa function wasn't handling zero value properly

--- ft_printf/srcs/ft_printf/ft_printf_uitoa.c
+++ ft_printf/srcs/ft_printf/ft_printf_uitoa.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 18:47:23 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/07/06 14:45:29 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/07/06 14:49:03 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int			ft_printf_uitoa(char *buff, uint64_t val)
 	int		size;
 
 	size = 0;
+	if (val == 0)
+	{
+		ft_strcinsert(buff, '0', 0);
+		return (1);
+	}
 	while (val > 0)
 	{
 		ft_strcinsert(buff, (val % 10) + '0', 0);
