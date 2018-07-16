8db41f6d75d08e536c7ab0ea64fb299531a6de94
Boris POPOVIC <bopopovi@student.42.fr>
2018-07-16 21:02:40 +0200

    [ADD] Function to append a character sequence multiple time to buffer

--- ft_printf/srcs/ft_printf/ft_printf_buffer.c
+++ ft_printf/srcs/ft_printf/ft_printf_buffer.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 18:48:52 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/07/11 21:12:21 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/07/16 20:57:49 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,35 @@ int			buff_append(t_buff *buff, char *input, int size)
 	{
 		buff->buff[buff->pos + i] = input[i];
 		i++;
+	}
+	buff->pos += i;
+	return (i);
+}
+
+int			buff_seqncat(t_buff *buff, char *input, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (buff->pos + (ft_strlen(input) * n) >= BUFF_SIZE)
+	{
+		write(1, buff->buff, buff->pos);
+		buff->read += buff->pos;
+		buff->pos = 0;
+		ft_bzero(buff->buff, BUFF_SIZE + 1);
+	}
+	while (n)
+	{
+		while (input[j])
+		{
+			buff->buff[buff->pos + i] = input[j];
+			j++;
+			i++;
+		}
+		j = 0;
+		n--;
 	}
 	buff->pos += i;
 	return (i);
