b96f76759c91720b3bf6f117562122245434843e
Boris POPOVIC <bopopovi@student.42.fr>
2018-07-26 16:36:44 +0200

    [MOD] Improved ft_wcslen function to return exact the length of wchar_t string once converted to utf-8, returns -1 if an unvalid character is encountered

--- ft_printf/srcs/ft_str/ft_wcslen.c
+++ ft_printf/srcs/ft_str/ft_wcslen.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 18:31:39 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/06/28 17:28:50 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/07/26 16:29:15 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,27 @@
 size_t		ft_wcslen(wchar_t *wcs)
 {
 	size_t	i;
+	size_t	bytes;
 
 	i = 0;
+	bytes = 0;
 	if (wcs)
 	{
 		while (wcs[i] != L'\0')
+		{
+			if ((wcs[i] > 255 && MB_CUR_MAX != 4) || wcs[i] < 0x0
+				|| (wcs[i] >= 0xd800 && wcs[i] <= 0xdfff) || wcs[i] > 0x10ffff)
+				return (-1);
+			if (wcs[i] > 65536)
+				bytes += 4;
+			else if (wcs[i] > 2048)
+				bytes += 3;
+			else if (wcs[i] > 128)
+				bytes += 2;
+			else
+				bytes++;
 			i++;
+		}
 	}
-	return (i);
+	return (bytes);
 }
