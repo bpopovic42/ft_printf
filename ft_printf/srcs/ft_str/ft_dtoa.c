f0109c40a0667d51651e78b6b64a6fc69fe4f4fb
Boris POPOVIC <bopopovi@student.42.fr>
2018-08-22 18:24:23 +0200

    [WIP] Simplified code and reduced number of lines

--- ft_printf/srcs/ft_str/ft_dtoa.c
+++ ft_printf/srcs/ft_str/ft_dtoa.c
@@ -6,109 +6,11 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/13 19:10:37 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/08/22 17:58:46 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/08/22 18:23:52 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void		get_decpart(t_dbl *dbl, int precision, char *buff)
-{
-	int tmp;
-
-	while (precision)
-	{
-		dbl->val /= 10;
-		dbl->val *= 100;
-		tmp = (int)dbl->val;
-		dbl->val -= tmp;
-		if (precision == 1)
-		{
-			dbl->val *= 10;
-			if ((int)dbl->val >= 5)
-				tmp += tmp < 9 ? 1 : 0;
-		}
-		ft_ccat(buff, tmp + '0');
-		precision--;
-	}
-}
-
-static int		write_intpart(double *val, char *buff, int i)
-{
-	double test;
-	int		ret;
-
-	test = *val;
-	ret = 0;
-	while (i)
-	{
-		ft_ccat(buff, (long long)(test) % 10 + '0');
-		test -= (long long)(test);
-		test /= 10;
-		test *= 100;
-		*val *= 10;
-		if (i < 0)
-			i++;
-		else
-			i--;
-		ret++;
-	}
-	*val /= 10;
-	*val -= (long long)*val;
-	return (ret);
-}
-
-static int		adjust(double *val)
-{
-	int i;
-
-	i = 0;
-	if (*val >= 10)
-	{
-		while (*val >= 10)
-		{
-			*val /= 10;
-			i++;
-		}
-	}
-	else if (*val < 1)
-	{
-		while (*val < 1)
-		{
-			*val *= 10;
-			i--;
-		}
-	}
-	return (i);
-}
-
-static int		get_intpart(t_dbl *dbl, int *precision, char *buff, char spec)
-{
-	int expn;
-
-	buff[0] = dbl->bits.sign ? '-' : buff[0];
-	dbl->bits.sign = 0;
-	if (!(ft_toupper(spec) == 'F' && (int)dbl->val == 0))
-		expn = adjust(&(dbl->val)) + (ft_strchr("fF", spec) ? 1 : 0);
-	else
-		expn = 1;
-	if ((*precision == 0 || (ft_toupper(spec) == 'G' && *precision == 1)) && (int)(dbl->val * 10) % 10 > 5)
-		dbl->val += 0.5;
-	if ((spec == 'G' || spec == 'g'))
-	{
-		if (expn > -4 && expn < *precision)
-			*precision -= write_intpart(&dbl->val, buff, *precision < expn ? *precision : expn);
-		else
-			*precision -= write_intpart(&dbl->val, buff, 1);
-	}
-	else
-		write_intpart(&dbl->val, buff, ft_strchr("fF", spec) ? expn : 1);
-	ft_ccat(buff, *precision ? '.' : '\0');
-	dbl->val -= (uint64_t)dbl->val;
-	return (expn);
-}
-
-
 
 static void round_dbl(char *buff)
 {
@@ -131,6 +33,79 @@ static void round_dbl(char *buff)
 	}
 }
 
+static int		write_intpart(double *val, char *buff, int i)
+{
+	double	tmp;
+	int		ret;
+
+	tmp = *val;
+	ret = 0;
+	while (i)
+	{
+		ft_ccat(buff, (long long)(tmp) % 10 + '0');
+		tmp -= (long long)(tmp);
+		tmp /= 10;
+		tmp *= 100;
+		*val *= 10;
+		if (i < 0)
+			i++;
+		else
+			i--;
+		ret++;
+	}
+	*val /= 10;
+	*val -= (long long)*val;
+	return (ret);
+}
+
+static int		adjust(double *val)
+{
+	int i;
+
+	i = 0;
+	if (*val > 9 || *val < 1)
+	{
+		while (*val > 9 || *val < 1)
+		{
+			if (*val > 9)
+			{
+				*val /= 10;
+				i++;
+			}
+			else
+			{
+				*val *= 10;
+				i--;
+			}
+		}
+	}
+	return (i);
+}
+
+static int		get_intpart(t_dbl *dbl, int *precision, char *buff, char spec)
+{
+	int expn;
+
+	buff[0] = dbl->bits.sign ? '-' : buff[0];
+	dbl->bits.sign = 0;
+	if (!(ft_toupper(spec) == 'F' && (int)dbl->val == 0))
+		expn = adjust(&(dbl->val)) + (ft_strchr("fF", spec) ? 1 : 0);
+	else
+		expn = 1;
+	if ((spec == 'G' || spec == 'g'))
+	{
+		if (expn > -4 && expn < *precision)
+			*precision -= write_intpart(&dbl->val, buff, *precision < expn ? *precision : expn);
+		else
+			*precision -= write_intpart(&dbl->val, buff, 1);
+	}
+	else
+		write_intpart(&dbl->val, buff, ft_strchr("fF", spec) ? expn : 1);
+	ft_ccat(buff, *precision ? '.' : '\0');
+	dbl->val -= (long long)dbl->val;
+	return (expn);
+}
+
 int			ft_dtoa(double val, int precision, char *buff, char spec)
 {
 	t_dbl		dbl;
@@ -142,16 +117,19 @@ int			ft_dtoa(double val, int precision, char *buff, char spec)
 		if (dbl.bits.mant)
 			ft_strcat(buff, "nan");
 		else
-		{
-			if (dbl.bits.sign)
-				ft_strcat(buff, "-");
-			ft_strcat(buff, "inf");
-		}
+			ft_strcat(buff, dbl.bits.sign ? "-inf" : "inf");
 		return (0);
 	}
 	expn = get_intpart(&dbl, &precision, buff + 1, spec);
-	get_decpart(&dbl, precision, buff + 1);
-	if ((int)(dbl.val) % 10 > 5)
+	while (precision)
+	{
+		dbl.val /= 10;
+		dbl.val *= 100;
+		ft_ccat(buff + 1, (int)dbl.val + '0');
+		dbl.val -= (int)dbl.val;
+		precision--;
+	}
+	if ((int)(dbl.val * 10) % 10 > 5)
 		round_dbl(buff);
 	return (expn);
 }
