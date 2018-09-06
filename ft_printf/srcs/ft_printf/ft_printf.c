ddebca4641e6566acf38ddf01049b9189d511ded
Boris POPOVIC <bopopovi@student.42.fr>
2018-09-06 17:36:30 +0200

    [ADD] Comment describing project specifications

--- ft_printf/srcs/ft_printf/ft_printf.c
+++ ft_printf/srcs/ft_printf/ft_printf.c
@@ -6,11 +6,21 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 15:26:54 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/04 16:14:05 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/06 17:35:34 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+** Printf version including 'aAcCdDeEfFgGinoOpsSuUxX' standard conversions
+** Non standard conversions are :
+** 'bB' for binary and 'r' for non-printable strings (octal form)
+** Flags implemented :
+** '-+# .hjlz*'
+** Include a color format handling (refer to ft_printf.h for details)
+** This printf version uses no malloc
+*/
 
 int			ft_printf(const char *restrict format, ...)
 {
